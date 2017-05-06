c36ad607966c53008a30686899086d2ac66da42a
Shadow53 <shadow53@shadow53.com>
2017-05-05 20:32:33 -0700

    Added example config file

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,9 +1,5 @@
-# To define a different place to build the zips, change $HOME
-# To define a different location to place them, change $DEST
-ROOT="$(pwd)"
-HOME="/home/mnbryant/shadow53.com/no-gapps-build/"
-DEST="/home/mnbryant/shadow53.com/downloads/no-gapps/"
-TMP="${HOME}tmp/"
-
+source ~/.config/android-build-scripts.sh
+# Make sure we have an updated update-binary
+cp "lib/update-binary" "${HOME}/update-binary"
 mkdir -p $TMP
 cd $TMP

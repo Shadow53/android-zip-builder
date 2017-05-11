bc77cc1b6b9073d371728013a997aa1fd69f4549
Shadow53 <shadow53@shadow53.com>
2017-05-10 21:27:50 -0700

    Testing on a remote server != push untested changes

--- lib/setup.sh
+++ lib/setup.sh
@@ -3,7 +3,7 @@ source ~/.config/android-build-scripts.sh
 # Create any missing directories
 if [ ! -d "$ROOT" ]; then mkdir -p $ROOT; fi
 if [ ! -d "$DEST" ]; then mkdir -p $DEST; fi
-if [ ! -d "$TMP" ]; then mkdir -p $TMP
+if [ ! -d "$TMP" ]; then mkdir -p $TMP; fi
 # Make sure we have an updated update-binary
 echo "Copying update-binary to workspace root"
 cp "${BASH_SOURCE%/*}/update-binary" "${ROOT}/update-binary"

48cd06600d1d4f4698225347014cb7ba39091481
Shadow53 <shadow53@shadow53.com>
2017-05-10 21:26:53 -0700

    Fix syntax

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,9 +1,9 @@
 # Include configuration
 source ~/.config/android-build-scripts.sh
 # Create any missing directories
-if [ ! -d "$ROOT" ]; then; mkdir -p $ROOT; fi
-if [ ! -d "$DEST" ]; then; mkdir -p $DEST; fi
-if [ ! -d "$TMP" ]; then; mkdir -p $TMP; fi
+if [ ! -d "$ROOT" ]; then mkdir -p $ROOT; fi
+if [ ! -d "$DEST" ]; then mkdir -p $DEST; fi
+if [ ! -d "$TMP" ]; then mkdir -p $TMP
 # Make sure we have an updated update-binary
 echo "Copying update-binary to workspace root"
 cp "${BASH_SOURCE%/*}/update-binary" "${ROOT}/update-binary"

784562ced379f49a128cddf92ae2206889a02bce
Shadow53 <shadow53@shadow53.com>
2017-05-10 21:24:27 -0700

    Only create nonexistant folders

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,9 +1,9 @@
 # Include configuration
 source ~/.config/android-build-scripts.sh
 # Create any missing directories
-mkdir -p $ROOT
-mkdir -p $DEST
-mkdir -p $TMP
+if [ ! -d "$ROOT" ]; then; mkdir -p $ROOT; fi
+if [ ! -d "$DEST" ]; then; mkdir -p $DEST; fi
+if [ ! -d "$TMP" ]; then; mkdir -p $TMP; fi
 # Make sure we have an updated update-binary
 echo "Copying update-binary to workspace root"
 cp "${BASH_SOURCE%/*}/update-binary" "${ROOT}/update-binary"

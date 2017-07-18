0765822bd6a5daffc1e6d416601e5d19600309fc
Shadow53 <shadow53@shadow53.com>
2017-07-18 11:54:25 -0700

    Include F-Droid script

--- lib/include_all.sh
+++ lib/include_all.sh
@@ -2,4 +2,5 @@ source "${BASH_SOURCE%/*}"/misc_functions.sh
 source "${BASH_SOURCE%/*}"/make_addond_script.sh
 source "${BASH_SOURCE%/*}"/add_default_perms.sh
 source "${BASH_SOURCE%/*}"/make_updater_script.sh
+source "${BASH_SOURCE%/*}"/fdroid.sh
 source "${BASH_SOURCE%/*}"/setup.sh
